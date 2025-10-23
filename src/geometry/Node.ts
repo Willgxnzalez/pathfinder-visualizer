1315ce67dc42c0206425d386815a3903361e16be
Wilberth Gonzalez <willgxnzalez@icloud.com>
2025-10-23 10:56:01 -0700

    Add Node class implementing INode interface

---
+++ src/geometry/Node.ts
@@ -0,0 +1,27 @@
+import { INode } from "../types";
+
+export class Node implements INode {
+    id: string;
+    walkable: boolean;
+
+    // Map-specific
+    lat: number;
+    lng: number;
+
+    gCost: number = Infinity;
+    hCost: number = 0;
+    fCost: number = 0;
+    parent: INode | null = null;
+
+    isStart: boolean = false;
+    isEnd: boolean = false;
+    isVisited: boolean = false;
+    isPath: boolean = false;
+
+    constructor(id: string, lat: number, lng: number, walkable: boolean) {
+        this.id = id;
+        this.lat = lat;
+        this.lng = lng;
+        this.walkable = walkable;
+    }
+}
