fef4792982c0a749d3854764adbcb1baf0e28734
Wilberth Gonzalez <willgxnzalez@icloud.com>
2025-11-05 03:57:51 -0800

    Optimize GridView event handlers with useCallback

--- src/components/GridView.tsx
+++ src/components/GridView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useCallback } from "react";
 import Grid from "../models/Grid";
 
 interface GridViewProps {
@@ -15,48 +15,71 @@ export default function GridView({ grid, onDrawingChange }: GridViewProps) {
         return () => grid.destroy();
     }, [grid]);
 
+    const handleMouseDown = useCallback((e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        onDrawingChange?.(true);
+        grid.handleMouseDown(
+            e.clientX,
+            e.clientY,
+            e.shiftKey && e.button === 0,
+            e.shiftKey && e.button === 2
+        );
+    }, [grid, onDrawingChange]);
+
+    const handleMouseMove = useCallback((e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+         grid.handleMouseMove(e.clientX, e.clientY);
+    }, [grid]);
+
+    const handleMouseUp = useCallback(() => {
+        onDrawingChange?.(false);
+        grid.handleMouseUp();
+    }, [grid, onDrawingChange]);
+
+    const handleMouseLeave = useCallback(() => {
+        onDrawingChange?.(false);
+        grid.handleMouseUp();
+    }, [grid, onDrawingChange]);
+
+    const handleTouchStart = useCallback((e: React.TouchEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        const touch = e.touches[0];
+        onDrawingChange?.(true);
+        grid.handleMouseDown(
+            touch.clientX,
+            touch.clientY,
+            false,
+            false
+        );
+    }, [grid, onDrawingChange]);
+
+    const handleTouchMove = useCallback((e: React.TouchEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        const touch = e.touches[0];
+        grid.handleMouseMove(touch.clientX, touch.clientY);
+    }, [grid]);
+
+    const handleTouchEndOrCancel = useCallback((e: React.TouchEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        onDrawingChange?.(false);
+        grid.handleMouseUp();
+    }, [grid, onDrawingChange]);
+
+    const handleContextMenu = useCallback((e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.preventDefault();
+    }, []);
+
     return (
-        <div className="absolute top-0 left-1/2 -translate-x-1/2 z-0 flex justify-center items-center grid-background select-none cursor-crosshair touch-none"
+        <div
+            className="relative select-none cursor-crosshair touch-none"
             ref={containerRef}
-            onMouseDown={(e) => {
-                onDrawingChange?.(true);
-                grid.handleMouseDown(
-                    e.clientX,
-                    e.clientY,
-                    e.shiftKey && e.button === 0,
-                    e.shiftKey && e.button === 2
-                );
-            }}
-            onMouseMove={(e) => grid.handleMouseMove(e.clientX, e.clientY)}
-            onMouseUp={() => { onDrawingChange?.(false); grid.handleMouseUp(); }}
-            onMouseLeave={() => { onDrawingChange?.(false); grid.handleMouseUp(); }}
-            onTouchStart={(e) => {
-                e.preventDefault(); // Prevent scrolling while drawing
-                const touch = e.touches[0];
-                onDrawingChange?.(true);
-                grid.handleMouseDown(
-                    touch.clientX,
-                    touch.clientY,
-                    false, // Touch events don't have shiftKey
-                    false
-                );
-            }}
-            onTouchMove={(e) => {
-                e.preventDefault(); // Prevent scrolling while drawing
-                const touch = e.touches[0];
-                grid.handleMouseMove(touch.clientX, touch.clientY);
-            }}
-            onTouchEnd={(e) => {
-                e.preventDefault();
-                onDrawingChange?.(false);
-                grid.handleMouseUp();
-            }}
-            onTouchCancel={(e) => {
-                e.preventDefault();
-                onDrawingChange?.(false);
-                grid.handleMouseUp();
-            }}
-            onContextMenu={(e) => e.preventDefault()}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
+            onMouseLeave={handleMouseLeave}
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEndOrCancel}
+            onTouchCancel={handleTouchEndOrCancel}
+            onContextMenu={handleContextMenu}
         />
     );
 }
