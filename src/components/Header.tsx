e09d0aae098e60ec2f82854d74577fb88fc4bde5
Wilberth Gonzalez <willgxnzalez@icloud.com>
2026-01-16 19:35:25 -0800

    Update Header component title and adjust padding for improved layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,8 +12,8 @@ export default function Header({
 }: HeaderProps) {
     return (
         <header className='w-full flex flex-col bg-surface select-none shadow border-b border-bdr'>
-            <div className='flex justify-between items-center px-10 py-2 border-b border-bdr'>
-                <h1 className='text-3xl font-bold text-text-main'>PF Visualizer</h1>
+            <div className='flex justify-between items-center px-5 py-2 border-b border-bdr'>
+                <h1 className='text-3xl font-bold text-text-main'>PathFinding Visualizer</h1>
                 <div className='flex gap-3'>
                     <button onClick={onDarkModeToggle}>DarkMode</button>
                     <button onClick={onMapModeToggle}>MapMode</button>
